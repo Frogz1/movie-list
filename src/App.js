3e4e535463757614bf188369ece90b659b54675f
Trevor Old <trevor.old@outlook.com>
2018-02-04 01:18:47 -0800

    added input for adding movie

--- src/App.js
+++ src/App.js
@@ -41,34 +41,59 @@ class App extends Component {
     return (
 
       <div className="App">
+        
         <div className="page-header">
           <h1 className="header">Movie List</h1>
         </div>
-        <div className="container col-xs-offset-4 col-xs-4">
-        <div className="form-group">
-          <label >Search Movies</label>
-          <input  className="form-control"
-            type="text"
-            onChange={(e) => {
-            this.setState({query: e.target.value})
-            Search.componentWillUpdate;
-          }}
-            className="form-control"
-            id="movies"
-            placeholder=''/>
+        <div className="container">
 
-        </div>
+          <form style={{margin: '10px', float: 'left'}} className="form-inline">
+            <div className="form-group">
+              <div className=" col-md-6 input-group">
+              <input type="text" className="form-control" placeholder="add movie title here"/> 
+              </div>
+          </div>
+          <button type="submit" className="btn btn-default">Add Movie</button>
+      </form>
+      <form style={{margin: '10px'}} className="form-inline">
+
+      <div className=" col-md-4 input-group">        
+            <input  className="form-control" type="text" placeholder="search...." onChange={(e) => { this.setState({query: e.target.value})
+                Search.componentWillUpdate; }} className="form-control" id="movies" />
+                </div>
+
+
+                
+                <button className="btn btn-default " type="button">
+                Go !!!
+                </button>
+
+
+          </form>
+          </div>
+          <div className="container">
+
+
+
         
-          <Jumbotron >
 
-            <ul className="list-group">
+
+        
+        
+          <Jumbotron>
+
+            <ul className="list-group"> 
             
               <Search movies={this.state.movies} query={this.state.query} modalToggle={this.toggleModal.bind(this)}/>
 
             </ul>
           </Jumbotron>
+          </div>
         </div>
-      </div>
+
+
+       
+
     );
   }
 }
