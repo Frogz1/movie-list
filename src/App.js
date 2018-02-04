9736a947c246d2b25dc58758c1b71884c7df6afa
Trevor Old <trevor.old@outlook.com>
2018-02-04 14:45:59 -0800

    Added React Bootstrap support

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Navbar, Jumbotron, Button} from 'react-bootstrap';
+import {Navbar, Jumbotron, Button, ListGroup, ListGroupItem, FormControl, FormGroup, ButtonGroup, InputGroup, InputGroupButton} from 'react-bootstrap';
 import Search from './Search';
 
 class App extends Component {
@@ -38,18 +38,9 @@ class App extends Component {
     })
   }
   addMovie() {
-    // var movies = this.state.movies
-    // console.log(movies);
-
-    // movies.push({'title': this.state.newTitle})
-    // console.log(movies)
-    // this.setState({
-    //   movies : this.movies.slice()
-    //   // movies: newMovie
-
-    // })
-    
-
+    // var movies = this.state.movies console.log(movies); movies.push({'title':
+    // this.state.newTitle}) console.log(movies) this.setState({   movies :
+    // this.movies.slice()   // movies: newMovie })
 
   }
 
@@ -57,73 +48,80 @@ class App extends Component {
     return (
 
       <div className="App">
-        
+      
         <div className="page-header">
           <h1 className="header">Movie List</h1>
         </div>
         <div className="container">
 
-          <form style={{margin: '10px', float: 'left'}} className="form-inline">
-            <div className="form-group">
-              <div className=" col-md-6 input-group">
-              <input onChange={(e) => this.setState({
-                newTitle: e.target.value
-              })}
-                  type="label" ref="newtitle" className="form-control" placeholder="add movie title here"/> 
-              </div>
-          </div>
-          <button onClick={() => {
-            var movies = this.state.movies
-    console.log(movies);
-
-    movies.push({'title': this.state.newTitle})
-    this.setState({
-      movies: movies,
-      newTitle: ''
-    })
-    this.refs.newtitle.value = ''
-
-            
-            }} type="button" className="btn btn-default">Add Movie</button>
-      </form>
-      <form style={{margin: '10px'}} className="form-inline">
-
-      <div className=" col-md-4 input-group">        
-            <input  className="form-control" type="text" placeholder="search...." onChange={(e) => { this.setState({query: e.target.value})
-                Search.componentWillUpdate; }} className="form-control" id="movies" />
-                </div>
-
-
-                
-                <button className="btn btn-default " type="button">
-                Go !!!
-                </button>
-
-
+          <form
+            style={{margin: '10px' }} className="form-inline">
+          <FormGroup>
+          <InputGroup>
+          <FormControl
+                  onChange={(e) => this.setState({newTitle: e.target.value})}
+                  type="text"
+                  ref="newtitle"
+                  inputRef ={ ref => this.input = ref }
+                  placeholder="add movie title here" >
+                  </FormControl>
+                  <InputGroup.Button>
+            <Button
+              onClick={() => {
+              var movies = this
+                .state
+                .movies
+              movies.push({'title': this.state.newTitle});
+              this.setState({movies: movies, newTitle: ''});
+              this.input.value = ''
+            }}  type="button">Add Movie</Button>
+              </InputGroup.Button>
+              </InputGroup>
+            </FormGroup>
           </form>
-          </div>
-          <div className="container">
+          <form style={{
+            margin: '10px'
+          }} className="form-inline">
+          <FormGroup>
+          <InputGroup>
+              <FormControl
+                type="text"
+                placeholder="search...."
+                inputRef={ ref => this.input = ref}
+                onChange={(e) => {
+                this.setState({query: e.target.value})
+                Search.componentWillUpdate;
+              }}
+                className="form-control"
+                id="movies">
+                </FormControl>
 
+            <InputGroup.Button>
+            <Button onClick={ () =>
+                this.input.value = ''}>
+              Go !!!
+            </Button>
+            </InputGroup.Button>
+            </InputGroup>
+            </FormGroup>
+          </form>
 
-
-        
-
-
-        
-        
+        <div className="container">
           <Jumbotron>
-
-            <ul className="list-group"> 
-            
-              <Search movies={this.state.movies} query={this.state.query} modalToggle={this.toggleModal.bind(this)}/>
-
-            </ul>
+            <ListGroup className="list-group">
+              <Search
+                movies={this.state.movies}
+                query={this.state.query}
+                modalToggle={this
+                .toggleModal
+                .bind(this)}/>
+            </ListGroup>
           </Jumbotron>
-          </div>
         </div>
+      </div>
+      </div>
 
 
-       
 
     );
   }
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,29 +1,32 @@
 import React, { Component }  from 'react';
-import keydown from 'react-keydown';
-import { ModalBody, Modal, ModalHeader } from 'react-bootstrap';
+
+import { ListGroupItem } from 'react-bootstrap';
 var Search = (props) => {
      this.modal = {
         show: false
         
     }
-    this.toggleMe = () =>{
+    this.toggleMe = () =>
+        {
         this.modal.show = !this.modal.show
         }   
     
 
     
 
-    var titles = props.movies.map((ele, ind) =>{
+    var titles = props.movies.map((ele, ind) => {
 
-        if (ind > 4 || props.movies.length === 5)
+        if (ind > 4 || props.movies.length === 5){
 
         return ele.title.toLowerCase().indexOf(props.query.toLowerCase()) !== -1 ?
            <li key={ind} className="list-group-item" > {  ele.title  }</li> : 
        null;
+        }
+        
     });
 
     if (titles.filter((x,y) => x !== null).length < 1) {
-        return <li className="list-group-item list-group-item-danger" style={{color: 'red', border: 'none'}} >no movie by that name found..<br /><em>please try again</em></li>
+        return <ListGroupItem className="list-group-item list-group-item-danger" style={{color: 'red', border: 'none'}} >no movie by that name found..<br /><em>please try again</em></ListGroupItem>
             
     } else {
         return titles
